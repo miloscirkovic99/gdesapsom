4f7f6b45288e55f325c6f57997b3d13fd1cc94b3
unknown <cirkovicmilos99@gmail.com>
2025-02-04 23:53:54 +0100

    add file size validator

--- apps/gde-sa-psom-portal/src/app/core/validators/file-size-valdiator.ts
+++ apps/gde-sa-psom-portal/src/app/core/validators/file-size-valdiator.ts
@@ -2,10 +2,11 @@ import { AbstractControl, ValidatorFn } from '@angular/forms';
 
 export function fileSizeValidator(maxSize: number): ValidatorFn {
   return (control: AbstractControl) => {
-    const file = control.value;
+    const file = control.value as File;
+
     if (file && file.size > maxSize) {
-      return { fileSize: true };
+      return { fileSize: true }; // Return validation error
     }
-    return null;
+    return null; // No error
   };
 }
