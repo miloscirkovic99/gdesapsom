e2c007ad2056958df9956fe2a0c608fd5769de7c
Milos Cirkovic <milos.cirkovic@naissus.tech>
2025-02-06 14:39:01 +0100

    add snackbar for email sent

--- apps/gde-sa-psom-portal/src/app/shared/components/contact-form/contact-form.service.ts
+++ apps/gde-sa-psom-portal/src/app/shared/components/contact-form/contact-form.service.ts
@@ -1,22 +1,41 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
+import { SnackbarService } from '../../../core/services/snackbar.service';
+import { TranslocoService } from '@ngneat/transloco';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ContactFormService {
-  private http=inject(HttpClient)
-  constructor() { }
+  private http = inject(HttpClient);
+  private snackbarService = inject(SnackbarService);
+  private translocoService = inject(TranslocoService);
+  constructor() {}
 
-  sendEmail(data:any){
-    this.http.post<any>('gmail',data).subscribe({
-      next:(result)=>{
+  sendEmail(data: any) {
+    const translatedActionButton = this.translocoService.translate('close');
+
+    this.http.post<any>('gmail', data).subscribe({
+      next: (result) => {
+        const translatedMessage =
+          this.translocoService.translate('email_success');
+
+        this.snackbarService.openSnackbar(
+          translatedMessage,
+          translatedActionButton,
+          'success-snackbar'
+        );
       },
-      error:(err)=>{
-        console.error(err);
-        
-      }
-    })
-    
+      error: (err) => {
+        const translatedMessage =
+          this.translocoService.translate('error_global');
+
+        this.snackbarService.openSnackbar(
+          translatedMessage,
+          translatedActionButton,
+          'error-snackbar'
+        );
+      },
+    });
   }
 }
