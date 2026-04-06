3299af2d1d42f9487a39ba56160955595c980a31
miloscirkovic99 <cirkovicmilos99@gmail.com>
2026-04-06 16:55:01 +0200

    fix

--- apps/gde-sa-psom-portal/src/env/env.dev.ts
+++
@@ -1,9 +0,0 @@
-
-export const environment = {
-    baseUrl: 'http://localhost:4200',
-    apiUrl:'https://dev.gdesapsom.com/',
-    production: false,
-    cookieDomain: 'localhost',
-    googleAnalyticsId:"G-BW5JF0HZ5Z"
-  };
-
--- apps/gde-sa-psom-portal/src/env/env.qa.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  baseUrl: 'https://dev.gdesapsom.com',
-  apiUrl: 'https://dev.gdesapsom.com/',
-  production: false,
-  cookieDomain: '318j122.mars1.mars-hosting.com',
-  googleAnalyticsId: 'G-BW5JF0HZ5Z',
-  openCageAPIKEY:'35a9068c901341efbe8b843d1c022625'
-
-};
